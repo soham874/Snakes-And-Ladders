8c77b724f623dca1e7f159775540213d10000322
soham874 <soham874@gmail.com>
2020-12-21 18:31:26 +0530

    Added : Initialised the player position at 0

--- MainGame.js
+++ MainGame.js
@@ -1,1 +1,5 @@
 console.log("Welcome to snakes and ladders!!")
+
+let utility = require('./Utility.js')
+
+utility.playGame()
---
+++ Utility.js
@@ -0,0 +1,12 @@
+class Utility{
+
+    //function to play the game
+    playGame(){
+
+        var position = 0
+
+    }
+
+}
+
+module.exports = new Utility();
